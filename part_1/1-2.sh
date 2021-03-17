6c0e2e319f58a7d164bedc98a96c81dce1895c00
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2021-03-17 14:08:03 +0000

    add 1-2

--- part_1/1-2.sh
+++ part_1/1-2.sh
@@ -4,6 +4,11 @@
 # We’ve left containers and a image that won’t be used anymore and are taking space, as docker ps -as and docker images will reveal.
 #
 # Clean the docker daemon from all images and containers.
-#
+docker container stop $(docker container ls -q)
+docker container rm $(docker container ls -a -q)
+docker image rm $(docker images -a -q)
+
 # Submit the output for docker ps -a and docker images
+docker ps -a
+docker images
 
