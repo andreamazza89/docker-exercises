9b533cbe965ecacb8581da366cb8f38eefe6105b
priyapatil1 <patil.d.priya@gmail.com>
2021-03-25 17:54:32 +0000

    Priya - exercises 1-1 & 1-2

--- part_1/1-1.sh
+++ part_1/1-1.sh
@@ -4,8 +4,12 @@
 # Since we already did “Hello, World!” in the material let’s do something else.
 #
 # Start 3 containers from image that does not automatically exit, such as nginx, detached.
+docker container run --name a1 -d nginx
+docker container run --name b2 -d nginx
+docker container run --name c3 -d nginx
 #
 # Stop 2 of the containers leaving 1 up.
+docker container stop a1 b2
 #
 # Submit the output for docker ps -a which shows 2 stopped containers and one running.
-
+docker container ls -a
--- part_1/1-2.sh
+++ part_1/1-2.sh
@@ -4,6 +4,11 @@
 # We’ve left containers and a image that won’t be used anymore and are taking space, as docker ps -as and docker images will reveal.
 #
 # Clean the docker daemon from all images and containers.
+docker container stop c3
+docker container prune
+docker image rm nginx
+
 #
 # Submit the output for docker ps -a and docker images
-
+docker ps -a
+docker image ls -a
