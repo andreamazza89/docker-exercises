41e0b248f74e1d817086ea1ce698a54f25eec128
Sam Jones <sam.jones494@googlemail.com>
2021-03-25 23:02:24 +0000

    week 1

--- part_1/1-1.sh
+++ part_1/1-1.sh
@@ -5,7 +5,20 @@
 #
 # Start 3 containers from image that does not automatically exit, such as nginx, detached.
 #
+docker run -d --name one nginx
+docker run -d --name two nginx
+docker run -d --name three nginx
+
 # Stop 2 of the containers leaving 1 up.
 #
-# Submit the output for docker ps -a which shows 2 stopped containers and one running.
+docker stop one
+docker stop two
 
+# Submit the output for docker ps -a which shows 2 stopped containers and one running.
+docker container ps -a
+
+# CONTAINER ID        IMAGE                               COMMAND                  CREATED              STATUS                          PORTS                      NAMES
+# c22cc40cff52        nginx                               "/docker-entrypoint.…"   About a minute ago   Up About a minute               80/tcp                     three
+# 3cc7f6c43f1c        nginx                               "/docker-entrypoint.…"   About a minute ago   Exited (0) About a minute ago                              two
+# 34cac8e95051        nginx                               "/docker-entrypoint.…"   About a minute ago   Exited (0) About a minute ago                              one
+# 3724c87c3951        timescale/timescaledb:latest-pg12   "docker-entrypoint.s…"   9 months ago         Up 24 hours                     127.0.0.1:5432->5432/tcp   postgres
--- part_1/1-2.sh
+++ part_1/1-2.sh
@@ -4,6 +4,18 @@
 # We’ve left containers and a image that won’t be used anymore and are taking space, as docker ps -as and docker images will reveal.
 #
 # Clean the docker daemon from all images and containers.
+
+docker container stop three
+docker container prune -f
+docker image rm nginx
 #
 # Submit the output for docker ps -a and docker images
 
+docker ps -a
+docker image ls -a
+
+# CONTAINER ID        IMAGE                               COMMAND                  CREATED             STATUS              PORTS                      NAMES
+# 3724c87c3951        timescale/timescaledb:latest-pg12   "docker-entrypoint.s…"   9 months ago        Up 24 hours         127.0.0.1:5432->5432/tcp   postgres
+
+# REPOSITORY              TAG                 IMAGE ID            CREATED             SIZE
+# timescale/timescaledb   latest-pg12         62850f3b364f        9 months ago        169MB
