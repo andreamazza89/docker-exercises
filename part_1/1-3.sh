965121c31f1a7e47d49d8c6d7bfd7fafd10933e8
Marion LV <marion.lv@8light.com>
2021-03-26 10:07:10 +0100

    Add Week 2 exercises and admin info

---
+++ part_1/1-3.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# 1.3: Secret message
+# Now that we’ve warmed up it’s time to get inside a container while it’s running!
+
+# Image devopsdockeruh/simple-web-service:ubuntu will start a container that outputs logs into a file. Go inside the container and use tail -f ./text.log to follow the logs. Every 10 seconds the clock will send you a “secret message”.
+
+# Submit the secret message and command(s) given as your answer.
