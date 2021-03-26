cc180fd2890d40924de3b7988b0c361ac71582a0
andrewMacmurray <a.macmurray@icloud.com>
2021-03-26 08:14:09 +0000

    adds week one

--- part_1/1-1.sh
+++ part_1/1-1.sh
@@ -5,7 +5,14 @@
 #
 # Start 3 containers from image that does not automatically exit, such as nginx, detached.
 #
+docker container run -d --name one nginx
+docker container run -d --name two nginx
+docker container run -d --name three nginx
+
 # Stop 2 of the containers leaving 1 up.
 #
+docker container stop one
+docker container stop two
+
 # Submit the output for docker ps -a which shows 2 stopped containers and one running.
 
