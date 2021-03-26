246e745e41aa6010b2ccd9f28ed04e9509151a4e
Elle Hallal <33027271+ellehallal@users.noreply.github.com>
2021-03-26 08:23:50 +0000

    Complete exercises 1-1 and 1-2

--- part_1/1-1.sh
+++ part_1/1-1.sh
@@ -5,7 +5,18 @@
 #
 # Start 3 containers from image that does not automatically exit, such as nginx, detached.
 #
+docker run -d --name container-one nginx
+docker run -d --name container-two nginx
+docker run -d --name container-three nginx
+#
 # Stop 2 of the containers leaving 1 up.
 #
+docker stop container-one
+docker stop container-two
+#
 # Submit the output for docker ps -a which shows 2 stopped containers and one running.
-
+#
+# CONTAINER ID   IMAGE                                              COMMAND                  CREATED              STATUS                          PORTS                                                                                                          NAMES
+# cdf53fea45f7   nginx                                              "/docker-entrypoint.…"   About a minute ago   Up About a minute               80/tcp                                                                                                         container-three
+# 67bb69f2c35f   nginx                                              "/docker-entrypoint.…"   About a minute ago   Exited (0) About a minute ago                                                                                                                  container-two
+# 0607965d32fe   nginx                                              "/docker-entrypoint.…"   About a minute ago   Exited (0) About a minute ago                                                                                                                  container-one
--- part_1/1-2.sh
+++ part_1/1-2.sh
@@ -5,5 +5,14 @@
 #
 # Clean the docker daemon from all images and containers.
 #
+docker stop container-three
+docker container prune
+docker image prune
+#
 # Submit the output for docker ps -a and docker images
-
+#
+docker ps -a
+docker images
+#
+# CONTAINER ID   IMAGE     COMMAND   CREATED   STATUS    PORTS     NAMES
+# REPOSITORY   TAG       IMAGE ID   CREATED   SIZE
