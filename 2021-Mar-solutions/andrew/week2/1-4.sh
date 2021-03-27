498ea39950acc2003064501e2c86f82bca1cf73a
andrewMacmurray <a.macmurray@icloud.com>
2021-03-27 18:23:59 +0000

    week 2 solutions

--- 2021-Mar-solutions/andrew/week2/1-4.sh
+++ 2021-Mar-solutions/andrew/week2/1-4.sh
@@ -4,6 +4,11 @@
 # Start a ubuntu image with the process sh -c 'echo "Input website:"; read website; echo "Searching.."; sleep 1; curl http://$website;'
 
 # You will notice that a few things required for proper execution are missing. Be sure to remind yourself which flags to use so that the read actually waits for input.
+NAME=website
+
+docker container run -it -d --rm --name $NAME ubuntu
+docker container exec -it $NAME sh -c 'apt update && apt upgrade && apt install curl -y'
+docker container exec -it $NAME sh -c 'echo "Input website:"; read website; echo "Searching.."; sleep 1; curl -L $website;'
 
 # > Note also that curl is NOT installed in the container yet. You will have to install it from inside of the container.
 
