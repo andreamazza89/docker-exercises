a16916e25c67f1c71adb34b0812973122cbf78ec
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2021-03-30 11:02:59 +0100

    solve for 1-3

--- part_1/1-3.sh
+++ part_1/1-3.sh
@@ -5,5 +5,14 @@
 
 # Image devopsdockeruh/simple-web-service:ubuntu will start a container that outputs logs into a file. Go inside the container and use tail -f ./text.log to follow the logs. Every 10 seconds the clock will send you a “secret message”.
 
+CONTAINER=$(docker run -d --rm devopsdockeruh/simple-web-service:ubuntu)
+sleep 15
+
+docker exec $CONTAINER tail -n 100 ./text.log | grep "message" | head -1
+
+docker stop $CONTAINER > /dev/null
+
 # Submit the secret message and command(s) given as your answer.
 
+# ➜ ./1-3.sh
+# Secret message is: 'You can find the source code here: https://github.com/docker-hy'
