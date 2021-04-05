9551856bba852b839270a6e0e0533000b86facd4
andrea mazzarella <andrea.mazzarella89@gmail.com>
2021-04-05 16:02:24 +0100

    week 3

---
+++ 2021-Mar-solutions/andrea/week3/1-6.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# Run docker run -it devopsdockeruh/pull_exercise
+
+# It will wait for your input
+# Navigate through docker hub to find the docs and Dockerfile that was used to create the image
+
+# Read the Dockerfile and/or docs to learn what input will get the application to answer a “secret message”
+
+# Submit the secret message and command(s) given to get it as your answer.
+
+echo "basics" | docker run --rm -i devopsdockeruh/pull_exercise
+
+# "This is the secret message"
