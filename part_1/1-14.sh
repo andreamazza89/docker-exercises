7bbd7bf99d0e489e93456596802dc74c6c3c1452
Merce Bauza <merce.bauza@gmail.com>
2021-05-18 08:38:05 +0100

    add-week-7

---
+++ part_1/1-14.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# 1.14: Environment
+
+# Start both frontend-example and backend-example with correct ports exposed and add ENV to Dockerfile with necessary information from both READMEs (front,back).
+
+# Ignore the backend configurations until frontend sends requests to _backend_url_/ping when you press the button.
+
+# You know that the configuration is ready when the button for 1.14 of frontend-example responds and turns green.
+
+# Do not alter the code of either project
+
+# Submit the edited Dockerfiles and commands used to run.
+
+
+# The frontend will first talk to your browser. Then the code will be executed from your browser and that will send a message to backend.
+
+# TIP: When configuring web applications keep browser developer console ALWAYS open, F12 or cmd+shift+I when the browser window is open. Information about configuring cross origin requests is in README of the backend project.
+
+# TIP: Developer console has multiple views, most important ones are Console and Network. Exploring the Network tab can give you a lot of information on where messages are being sent and what is received as response!
