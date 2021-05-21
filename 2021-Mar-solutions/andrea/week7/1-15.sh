1d8bd542f5686a5ded6c0f0292785cdf9d6e9100
andrea mazzarella <andrea.mazzarella89@gmail.com>
2021-05-21 08:49:29 +0200

    week 7

---
+++ 2021-Mar-solutions/andrea/week7/1-15.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# 1.15: Homework
+
+# Create Dockerfile for an application or any other dockerised project in any of your own repositories and publish it to Docker Hub. This can be any project except clones / forks of backend-example or frontend-example.
+
+# For this exercise to be complete you have to provide the link to the project in docker hub, make sure you at least have a basic description and instructions for how to run the application in a README that’s available through your submission.
+
+# here is the link: https://hub.docker.com/repository/docker/andreamazzarella/lambda_terra_deployer
