9baf6673df14a9ee895f169d9a877c6ec2f9151f
andrea mazzarella <andrea.mazzarella89@gmail.com>
2021-06-05 09:44:15 +0200

    adds week9 calendar/exercises

---
+++ part_2/2-4.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Add redis to example backend.
+#
+# Redis is used to speed up some operations. Backend uses a slow api to get information. You can test the slow api by requesting /ping?redis=true with curl. The frontend program has a button to test this.
+#
+# Configure a redis container to cache information for the backend. Use the documentation if needed when configuring: https://hub.docker.com/_/redis/
+#
+# The backend README should have all the information needed to connect.
+#
+# When you’ve correctly configured the button will turn green.
+#
+# Submit the docker-compose.yml
+
+# restart: unless-stopped can help if the redis takes a while to get ready
+
+# TIP: If you’re stuck check out tips and tricks
