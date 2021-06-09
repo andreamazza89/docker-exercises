47ea374dad21700ceb4840368d8bb2769413563b
andrea mazzarella <andrea.mazzarella89@gmail.com>
2021-06-09 12:15:22 +0200

    week 9

---
+++ 2021-Mar-solutions/andrea/week9/2-5.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# A project over at https://github.com/docker-hy/material-applications/tree/main/scaling-exercise has a hardly working application. Go ahead and clone it for yourself. The project already includes docker-compose.yml so you can start it by running docker-compose up.
+#
+# Application should be accessible through http://localhost:3000. However it doesn’t work well enough and I’ve added a load balancer for scaling. Your task is to scale the compute containers so that the button in the application turns green.
+#
+# This exercise was created with Sasu Mäkinen
+#
+# Please return the used commands for this exercise.
+
+docker-compose -f docker-compose.2_5.yml up --scale compute=5
